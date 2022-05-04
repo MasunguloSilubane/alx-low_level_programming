093d046ab6a86d5921138fef1c016c4f70de29ac
Silubane <masungulogodi3@gmail.com>
2022-05-04 11:25:31 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,12 +6,12 @@ int count_words(char *str);
 char **strtow(char *str);
 
 /**
- * * word_len - Locates the index marking the end of the
- * *            first word contained within a string.
- * * @str: The string to be searched.
- * *
- * * Return: The index marking the end of the initial word pointed to by str.
- * */
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
 int word_len(char *str)
 {
 	int index = 0, len = 0;
@@ -26,11 +26,11 @@ int word_len(char *str)
 }
 
 /**
- * * count_words - Counts the number of words contained within a string.
- * * @str: The string to be searched.
- * *
- * * Return: The number of words contained within str.
- * */
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The number of words contained within str.
+ */
 int count_words(char *str)
 {
 	int index = 0, words = 0, len = 0;
@@ -51,12 +51,12 @@ int count_words(char *str)
 }
 
 /**
- * * strtow - Splits a string into words.
- * * @str: The string to be split.
- * *
- * * Return: If str = NULL, str = "", or the function fails - NULL.
- * *         Otherwise - a pointer to an array of strings (words).
- * */
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ *
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *          Otherwise - a pointer to an array of strings (words).
+ */
 char **strtow(char *str)
 {
 	char **strings;
