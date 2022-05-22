6a0622c9e21be9fddd0b274377ce2cce56d900ab
Silubane <masungulogodi3@gmail.com>
2022-05-22 02:06:13 -0700

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * * main - Prints the opcodes of itself.
- * * @argc: The number of arguments supplied to the program.
- * * @argv: An array of pointers to the arguments.
- * *
- * * Return: Always 0.
- * */
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	int bytes, index;
