e7ad83adc3d8201ef75a0716ad5c041b9ff6b7cd
Silubane <masungulogodi3@gmail.com>
2022-05-23 01:55:36 -0700

    Task 01

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,6 @@
 *            a linked list_t list.
 * @h: The linked list_t list.
 * @elements: a counter var for size_t (nodes)
-*
 * Return: The number of elements in h.
 */
 size_t list_len(const list_t *h)
@@ -17,6 +16,5 @@ while (h)
 elements++;
 h = h->next;
 }
-
 return (elements);
 }
